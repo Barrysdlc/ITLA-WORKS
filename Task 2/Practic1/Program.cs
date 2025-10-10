83d35cc488ef54032610a89023a1aaf4fd32e843
Barrysdlc <cacarmona01@gmail.com>
2025-10-10 17:31:40 -0400

    Adding my second task

---
+++ Task 2/Practic1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class MySecondProgram
+{
+    static void Main()
+    {
+        
+        Console.WriteLine("=== Even or Odd Number Checker ===");
+
+        
+        Console.Write("Please enter a number: ");
+
+        
+        string input = Console.ReadLine();
+
+        
+        if (int.TryParse(input, out int number))
+        {
+            // Check if the number is even or odd
+            if (number % 2 == 0)
+            {
+                Console.WriteLine($"The number {number} is EVEN.");
+            }
+            else
+            {
+                Console.WriteLine($"The number {number} is ODD.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Error: Please enter a valid integer number.");
+        }
+
+        
+        Console.WriteLine("\nPress any key to exit...");
+        Console.ReadKey();
+    }
+}
+
+
